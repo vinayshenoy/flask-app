3b9f2e807bbd338d0f813b1846333971a31dc447
vinayshenoy <vinayshenoy98@gmail.com>
2020-09-17 17:47:33 +0530

    display quotes from database

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+import pymysql.cursors
 
 app = Flask(__name__)
+connection = pymysql.connect(host='localhost',user='root',password='',db='webscrape',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/',)
-def add_5_6():
-    c=5+6
-    return render_template('test1.html',name=c)
+def display_quotes():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT quotes.id,author.name,quotes.quote from quotes,author WHERE quotes.author_id=author.id order by quotes.id"
+            cursor.execute(sql) 
+            quotes = cursor.fetchall()
+        return render_template('test1.html',data=quotes)
 
-@app.route('/vinay',)
-def displayVinay():
-    return "Vinay yolo"    
+    except pymysql.Error as err:
+            return render_template('test1.html',error=err)
+
 
 if __name__ =='__main__':
     print("name",__name__)
-    app.run('localhost',1234)
+    app.run('localhost',1234,debug=True)
